0f2156939d12dce37020f15faf3a9e2d73d32e49
Jacob Knaack <jacobknaack@Jacobs-MacBook-Pro-2.local>
2015-11-11 00:57:54 -0800

    trimmed down my code a bunch

--- javascript/game.js
+++ javascript/game.js
@@ -1,91 +1,64 @@
 var user = prompt("Please enter your name: ");
 console.log("Username: " + user);
 
-var questionsAsked = []; // Stores questions that have been asked
-var questionFeed = []; // Stores all the feedback for questions answered
+var questionsAsked = [
+	{Q: "Was I born in Seattle?", A: "no"},
+	{Q: "Was I born in the Southern United States?", A: "yes"},
+	{Q: "Was I born in Austin Texas?", A: "no"},
+	{Q: "Was I born in Atlanta Georgia?", A: "yes"},
+	{Q: "Am I the greatest javascript programmer?", A:"no"}
+]; // Stores questions and answers
+var questionFeed = [
+	{R:"Good Guess " + user + "! Next Question...", W:"Sorry " + user + " that's wrong. Try another...", N:"See ya"},
+	{R:"2 for 2 " + user + "! Let's do another...", W:"Well darn that's not right. Try another...", N: "You forgot to answer"},
+	{R:"That's right " + user + ". I was not.", W: "That place sucks, no...", N:"Don't feel like guessing?"},
+	{R:"Yur darn right I was!", W: "Sorry " + user + " but I actually was born there.", N:"just press buttons!"},
+	{R:"Hurray! You won " + user + "!!", W:"Well no, but thanks!", N:"The very last question, really....."}
+]; // Stores all the feedback for all questions and answers
+var qVis = []; //Arrays used to display questions and answers
+var fVis = []; 
 var questionNum = 1; // Keeps track of question number
+var questionsCorrect = 0; //keeps track of right answers
 var answer = ''; // Makes answers global
 
 function biQuestion(question) { // function for asking questions
 	answer = prompt(question);
-	questionsAsked.push(questionNum + ". " + question + ' ' + answer);
-	document.getElementById("questions").innerHTML = questionsAsked.join('<br/>');
+	qVis.push(questionNum + ". " + question + " " + answer);
+	document.getElementById("questions").innerHTML = qVis.join('<br/>');
 	console.log("Response to question: " + answer);
 }
 
 function feedbackRight() { // adds an alert when you answer correctly
-	questionFeed.push("Questions #" + questionNum + ": Correct!");
-	document.getElementById("feedback").innerHTML = questionFeed.join('<br/>');
+	fVis.push("Question #" + questionNum + ": Correct!");
+	document.getElementById("feedback").innerHTML = fVis.join('<br/>');
 	questionNum ++;
+	questionsCorrect ++;
 }
 
 function feedbackWrong() { // adds an alert when you answer incorrectly
-	questionFeed.push("Question #" + questionNum + ": Incorrent :(");
-	document.getElementById("feedback").innerHTML = questionFeed.join('<br/>');
+	fVis.push("Question #" + questionNum + ": Incorrent :(");
+	document.getElementById("feedback").innerHTML = fVis.join('<br/>');
+	questionNum ++;
 }
 
 if (user != null) { //if user wants to cancel, the next prompt won't appear
 
-biQuestion("Was I born in Seattle?");
-
-	//beginning of the if statements with answer keys
-	if (answer == "no" || answer == "No" || answer == "N" || answer == "n" && answer != null) {
+for (var i = 0; i < questionsAsked.length; i++) {
+	biQuestion(questionsAsked[i].Q);
+	if (answer.toLowerCase() == questionsAsked[i].A) {
 		feedbackRight();
-		alert("Good guess " + user + "! On to the next question!");
-		biQuestion("Was I born in the Southern United States?");
-
-		if (answer == "yes" || answer == "Yes" || answer == "Y" || answer == "y" && answer != null) {
-			feedbackRight();
-			alert("2 for 2 " + user + "! How about we try another!"); 
-			biQuestion("Was I born in Austin Texas?");
-
-			if (answer == "no" || answer == "No" || answer == "N" || answer == "n" && answer != null) {
-				feedbackRight();
-				alert("Correct! What are the odds " + user +"! Here's another...");
-				biQuestion("Was I born in Atlanta Georgia?");
-
-				if (answer == "yes" || answer == "yes" || answer == "Y" || answer == "y" && answer != null) {
-					feedbackRight();
-					alert("Yur darn right I was!");
-					biQuestion("Now for the Finale: Am I the greatest Javascript Programmer?");
-
-						if (answer == "no" || answer == "No" || answer == "N" || answer == "n" && answer != null) {
-							feedbackRight();
-							alert("Congrats " + user + "! You Won!! :D");
-			
-			//else statements for wrong guesses and if user no longer wants to play (clicking cancel)
-					} else if (answer == null) {
-						alert("Really " + user + "... You wait till the last question to quit.");
-					} else {
-						feedbackWrong();
-						alert("Hah, I don't think I'm even close to the best! You Lose :(");
-					}
-
-			} else if (answer == null) {
-				alert("Aww, you made it so far!");
-			} else {
-				feedbackWrong();
-				alert("I'm not bold enough for a double bluff. You Lose :(");
-			}
-
-		} else if (answer == null) {
-			alert("fine, don't finish...");
-		} else {
-			feedbackWrong();
-			alert("Ouch that one was kinda tricky... You Lose :(");
-		} 
-
+		alert(questionFeed[i].R);
 	} else if (answer == null) {
-		alert("see ya!");
+		alert(questionFeed[i].N);
+		break;
 	} else {
 		feedbackWrong();
-		alert("One is better than nothing, but You Lose :(");
-	} 
-
-} else if (answer == null) {
-	alert("It's just a guessing game...");
-} else {
-	feedbackWrong();
-	alert("Dang, couldn't even get one quesion right. You Lose :(");
+		alert(questionFeed[i].W);
+	}
 }
+	if (questionsCorrect >= 3) {
+		alert("You Win! You got more than 3 questions correct :D");
+	} else {
+		alert("Sorry " + user + ", you got less than 3 question correct. You Lose :(");
+	}
 }
